a4a4a7333fc7eae352d1b999dd2f7d1430c087a4
Lokeshwar-Chankapure <118004819+Lokeshwar-Chankapure@users.noreply.github.com>
2023-07-06 22:49:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit=my_fruit_list.append("jackfruit")
+add_my_fruit=fruit_load_test.append("jackfruit")
 
