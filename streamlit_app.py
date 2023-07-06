177cac2b00c5f18b6572b581b71b4d60efe1d5ea
Lokeshwar-Chankapure <118004819+Lokeshwar-Chankapure@users.noreply.github.com>
2023-07-06 11:55:03 +0530

    Update streamlit_app.py
    added the items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Now')
+streamlit.text('Omega 3 & Blueberry Oatmeal)
+streamlit.text('Kale,spinach & Rocket Smoothie')   
+streamlit.text('Hard-Boiled and Free-Range Egg')
